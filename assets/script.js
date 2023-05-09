f571ccb9bad4a58cbeec29f2359e41aabca014ca
Natasa <nkilibarda3@gmail.com>
2023-05-09 18:16:38 +1000

    fix display

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,9 @@ document.getElementById("search-form").onsubmit = (e) => {
       console.log(data);
 
       document.querySelector("#current-weather-data #city-name").innerHTML =
-        data.city.name + " " + data.list[0].dt_txt;
+        data.city.name +
+        " " +
+        new Date(data.list[0].dt_txt).toLocaleDateString("en-US");
       document.querySelector("#current-weather-data #temp").innerHTML =
         "Current Temp (F): " + data.list[0].main.temp.toFixed(1);
       document.querySelector(
@@ -26,10 +28,12 @@ document.getElementById("search-form").onsubmit = (e) => {
         .querySelector("#current-weather-data #icon")
         .appendChild(weatherIcon);
 
+      // getting index 7, 15, 23, 31, 39
       [1, 2, 3, 4, 5].forEach((idx) => {
-        const forecastData = data.list[idx * 8];
-        document.querySelector(`#day${idx} #date${idx}`).innerHTML =
-          forecastData.dt_txt;
+        const forecastData = data.list[idx * 8 - 1];
+        document.querySelector(`#day${idx} #date${idx}`).innerHTML = new Date(
+          forecastData.dt_txt
+        ).toLocaleDateString("en-US");
 
         const forecastWeatherIcon = document.createElement("img");
         forecastWeatherIcon.src = `http://openweathermap.org/img/wn/${forecastData.weather[0].icon}@2x.png`;
